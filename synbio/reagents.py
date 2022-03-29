757983b0501ac7af1b7d15cbdc7d21b960ec0846
jecalles <callesjonathan@gmail.com>
2022-03-29 14:54:20 -0700

    removes incomplete Reagent.__add__ method

--- synbio/reagents.py
+++ synbio/reagents.py
@@ -35,9 +35,6 @@ class Reagent:
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.name}')"
 
-    def __add__(self, other):
-        re
-
     @property
     def recipe(self) -> Dict[Reagent, float]:
         """
